02f6661f038cba9bb858da83d27f3b40238939a3
Talon <asd@asd.com>
2018-04-23 13:11:21 +0100

    lecture 6.

--- src/UNIT-2/lecture-6/lect6.py
+++ src/UNIT-2/lecture-6/lect6.py
@@ -1,5 +1,8 @@
+import random
+
 from Drunk import Drunk
 from Drunk import UsualDrunk
+from Drunk import ColdDrunk
 from Field import Field
 from Location import Location
 
@@ -18,8 +21,50 @@ def walk(f, d, steps):
     return start.distFrom(f.getLoc(d))
 
 
-patatino = UsualDrunk('patatino')
-f1 = Field()
-f1.addDrunk(patatino, Location(0,0))
-print(walk(f1, patatino, 15))
-print()
+def simWalks(numTrials, numSteps, drunkType):
+    """
+    :param numTrials: int >= 0, times we append a new "times 'd' moves"
+    :param numSteps: int >= 0, times 'd' moves
+    :param drunkType: subclass of Drunk
+    :return: list of final distances for each trial
+    """
+    homer = drunkType('drunko')
+    distances = []
+    for _ in range(numTrials):
+        f = Field()
+        f.addDrunk(homer, Location(0, 0))
+        distances.append(
+            round(walk(f, homer, numSteps), 2)
+        )
+    return distances
+
+
+def drunkTest(walkLengths, numTrials, drunkType):
+    """
+    :param walkLengths: tuple of different lengths of steps
+    :param numTrials: int >= 0, times we run tries
+    :param drunkType: subclass of Drunk
+    :return: void
+    """
+    for numSteps in walkLengths:
+        distances = simWalks(numTrials, numSteps, drunkType)
+        print(drunkType.__name__, 'random walk of', numSteps, 'steps')
+        print(' Mean =', round(
+            sum(distances) / len(distances), 4
+        ))
+        print(' Max =', max(distances),' Min =', min(distances))
+
+
+def simAll(walkLengths, numTrials, drunkKinds):
+    for dClass in drunkKinds:
+        drunkTest(walkLengths, numTrials, dClass)
+
+
+# patatino = UsualDrunk('patatino')
+# f1 = Field()
+# f1.addDrunk(patatino, Location(0,0))
+# print(walk(f1, patatino, 15))
+# print()
+
+
+drunkTest( (10, 100, 1000, 10000), 100, UsualDrunk )
